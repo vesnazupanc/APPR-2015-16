659ec2e3a2dd6173b86e79256783202a7c3c2c5a
Vesna Zupanc <vesnazupanc93@gmail.com>
2015-12-04 12:05:54 +0100

    Uvozila sem podatke iz spletne strani.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,20 +1,34 @@
 # 2. faza: Uvoz podatkov
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
-}
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine()
+require(dplyr)
+require(rvest)
+require(gsubfn)
 
-obcine <- uvozi.obcine()
+link <- "http://www.stat.si/StatWeb/glavnanavigacija/podatki/prikazistaronovico?IdNovice=4013"
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+stran <- html_session(link) %>% read_html(encoding = "UTF-8")
+tabele <- stran %>% html_nodes(xpath ="//table[@rules='all']")
+tabela1 <- tabele %>% .[[1]] %>% html_table() 
+tabela2 <- tabele %>% . [[2]] %>% html_table()
+
+names(tabela1)<- tabela1[1,]
+tabela1 = tabela1[-1,]
+tabela1 = tabela1[-9,]
+tabela1 = tabela1[-11,]
+Encoding(tabela1[[1]]) <- "UTF-8"
+tabela1[9,1] <- 'Povprečna starost umrlih - moški'
+tabela1[10,1] <- 'Povprečna starost umrlih - ženske'
+tabela1[11,1] <- 'Pričakovano trajanje življenja - moški'
+tabela1[12,1] <- 'Pričakovano trajanje življenja - ženske'
+names(tabela2)<- tabela2[1,]
+tabela2 = tabela2[-1,]
+Encoding(tabela2[[1]]) <- "UTF-8"
+
+tabela2[,2:5] <- apply(tabela2[,2:5], 2, . %>% gsub("\\.", "", .) %>%
+                                               gsub(",", ".", .) %>%
+                                               as.numeric())
+tabela1[,2:5] <- apply(tabela1[,2:5], 2, . %>% gsub("\\.", "", .) %>%
+                                               gsub(",", ".", .) %>%
+                                               as.numeric())
+
