91c74bbcfbdb15e6747ea4f7d5e1d1c638b4c277
Vesna <vesnazupanc93@gmail.com>
2015-12-13 18:00:17 +0100

    uvoz tabel

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,9 +1,23 @@
 # 2. faza: Uvoz podatkov
-
-
 require(dplyr)
 require(rvest)
 require(gsubfn)
+
+
+#umrli po občinah v letu 2014
+
+stolpci = c("Občina", "Umrli", "Umrli mlajši od 65 let", "Prezgonja umrljivost")
+uvozi.obcine <- function(){
+  read.csv2(file= "podatki/podatki-obcine.csv", skip=4, nrow=(216-5), col.names=stolpci, fileEncoding="cp1250")
+}
+
+umrli.obcine <- uvozi.obcine()
+write.csv(umrli.obcine,"umrli.obcine.csv",row.names=FALSE)
+
+
+
+
+##Uvoz podatkov iz članka na spletni strani stat.si
 
 link <- "http://www.stat.si/StatWeb/glavnanavigacija/podatki/prikazistaronovico?IdNovice=4013"
 
@@ -31,4 +45,9 @@ tabela2[,2:5] <- apply(tabela2[,2:5], 2, . %>% gsub("\\.", "", .) %>%
 tabela1[,2:5] <- apply(tabela1[,2:5], 2, . %>% gsub("\\.", "", .) %>%
                                                gsub(",", ".", .) %>%
                                                as.numeric())
+
+
+write.csv(tabela1,"tabela1.csv",row.names=FALSE)
+write.csv(tabela2,"tabela2.csv",row.names=FALSE)
+
 
