c090df9e618e506302f962324ef8914aed3f45c0
Vesna <vesnazupanc93@gmail.com>
2016-03-13 11:54:50 +0100

    dodane knjižnice

--- lib/libraries.r
+++ lib/libraries.r
@@ -5,9 +5,18 @@ require(dplyr)
 require(rvest)
 require(gsubfn)
 require(zoo)
+library(maptools)
+library(sp)
+library(digest)
+library(ggplot2)
+library(dplyr)
+library(MASS)
+library(mgcv)
 
 # Uvozimo funkcije za delo z datotekami XML.
 source("lib/xml.r", encoding = "UTF-8")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding = "UTF-8")
+
+
