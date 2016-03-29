524195c13b3be7353751ff688872186cdc99f1ab
Vesna <vesnazupanc93@gmail.com>
2016-03-29 23:20:39 +0200

    dokončala napredo analizo

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,7 +1,6 @@
 # 4. faza: Analiza podatkov
 
-#barve <- rainbow(length(levels(obcine[[7]])))
-#names(barve) <- levels(obcine[[7]])
+## GRUPIRANJE
 
 a <- inner_join(Prebivalci_regije, Umrli_regije)
 a <- data.frame(a %>% group_by(Regija) %>% summarise(St.Prebivalcev=sum(St.Prebivalcev), Umrli=sum(Umrli)))
@@ -9,10 +8,10 @@ rownames(a) <- a$Regija
 a <- a[,-1]
 
 
-n <- 3
+n <- 3 #REGIJE BOM ZDRUŽILA V 3 SKUPINE
 
-a.norm <- scale(a)
-k <- kmeans(a.norm, n, nstart = 1000)
+a.norm <- scale(a,scale=FALSE) #povprečje umrlih in prebivalcev ter tabela po regijah z odstopanji od povprečja..
+k <- kmeans(a.norm, n, nstart = 1000) #regije razdelim v 3 skupine glede na povprečja
 regije <- row.names(a)
 m <- match(zemljevid$NAME_1, regije)
 zemljevid$skupina <- factor(k$cluster[regije[m]])
@@ -30,7 +29,7 @@ ZEM1 <- ZEM1 + labs(x="", y="")+
   scale_x_continuous(breaks=NULL)+
   theme_minimal()
 
-print(ZEM1)
+
 
 ##CENTROIDI
 razdalje <- apply(k$centers, 1, function(y) apply(a.norm, 1, function(x) sum((x-y)^2)))
@@ -40,11 +39,7 @@ najblizje <- apply(manj.razdalje[,apply(manj.razdalje, 2, any)], 2, which)
 REG <- names(najblizje)[order(najblizje)]
 
 
-
-
-#graf: STAROST-ŠT.UMRLIH:
-
-
+## NAREDIM ANALIZO POVEZANOSTI MED STAROSTJO IN ŠTEVILOM UMRLIH:
 podatki <- umrli.starost %>% group_by(Starost) %>% summarise(Umrli = sum(Umrli))
 
 GRAF1 <- ggplot(podatki, aes(x=Starost, y=Umrli)) + 
@@ -52,7 +47,6 @@ GRAF1 <- ggplot(podatki, aes(x=Starost, y=Umrli)) +
   labs(title ="Št. umrlih, glede na starost")+
   theme_bw()
 
-print(GRAF1)
 
 ###
 model.loess <- loess(data = podatki, Starost ~ Umrli)
@@ -68,19 +62,15 @@ sum(model$residuals^2) # 60584308
 model <- lm(data = podatki, Umrli ~ Starost + I(Starost^2) + I(Starost^3) + I(Starost^4) + I(Starost^5) + I(Starost^6))
 sum(model$residuals^2) # 0.01712185
 
-GRAF1 + geom_smooth(method = "lm", formula = y ~ x + I(x^2) + I(x^3) + I(x^4) + I(x^5)+ I(x^6))
+GRAF1 <- GRAF1 + geom_smooth(method = "lm", formula = y ~ x + I(x^2) + I(x^3) + I(x^4) + I(x^5) + I(x^6))
 
 
-ggplot(podatki, aes(x = Starost, y = Umrli)) + xlim(0, 110) + geom_point() +
-  geom_smooth(method = "lm", formula = y ~ x + I(x^2) + I(x^3) + I(x^4) + I(x^5)+ I(x^6)+ I(x^7) + I(x^8), se = FALSE,
-              fullrange = TRUE, color = "green")
+
 
 ## št. umrlih narašča s starostjo potem pa pada - najverjetneje zato, ker je manj prebivalcev, starih nad 90let..
 
 ## Umrli glede na število prebivalcev oz. procent umrlih po starosti:
 ## Uvozim novo tabelo v uvoz.r za lažjo analizo in narišem graf, ki prikazuje procent umrlih glede na starost:
-
-
 
 mgam <- gam(data = TABELA3, Procent.umrlih ~ s(Starost))
 sum(mgam$residuals^2) #29.10676
@@ -97,9 +87,28 @@ GRAF2 <- ggplot(TABELA3, aes(x=Starost,y=Procent.umrlih))+
   geom_point(stat = "identity")+
   labs(title ="Procent umrlih glede na starost")+
   theme_bw()
-GRAF2 + geom_smooth(method = "gam", formula = y ~ s(x))
-print(GRAF2)
+GRAF2 <- GRAF2 + geom_smooth(method = "gam", formula = y ~ s(x))
 
-ggplot(TABELA3, aes(x = Starost, y = Procent.umrlih)) + geom_point() +
-  geom_smooth(method = "lm", formula = y ~ x + I(x^2) + I(x^3) + I(x^4) + I(x^5), se = FALSE,
-              fullrange = TRUE, color = "green")
+NAPOVED1 <- data.frame(Starost = c(100:115), Procent.umrlih = predict(mgam, data.frame(Starost=seq(100,115))))
+
+
+
+
+
+
+
+### NAREDIM NAPOVEDI, ZA POVPREČNO STAROSTI MOŠKIH IN ŽENSK V LETIH 2015:2025
+
+#NAPOVED ZA POVPREČNO STAROST SMRTI ZA ŽENSKE
+LMZ <- lm(data = filter(umrljivost, Spol == "Ženske"), Povprecna.starost ~ Leto)
+Z <- predict(LMZ, data.frame(Leto = seq(2015, 2025, 1)))
+#NAPOVED ZA POVPREČNO STAROST SMRTI ZA MOŠKE
+LMM <- lm(data = filter(umrljivost, Spol == "Moški"), Povprecna.starost ~ Leto)
+M <- predict(LMM, data.frame(Leto = seq(2015, 2025, 1)))
+
+NAPOVED2  <- data.frame("Ženske" = Z, "Moški" = M, row.names = (2015:2025))
+
+
+
+
+
